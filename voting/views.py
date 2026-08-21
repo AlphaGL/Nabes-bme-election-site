94ddc58cdf0979ded47e028483dd778ad09d3194
Ibeawuchi chukwugozirim <ibeawuchichukwugozirim@gmail.com>
2026-08-21 10:49:10 +0100

    update

--- voting/views.py
+++ voting/views.py
@@ -270,7 +270,7 @@ def register_student(request):
 
 
 class BmeLoginView(LoginView):
-    template_name = 'voting/login.html'
+    template_name = 'voting/login_closed.html'
     form_class = AuthenticationForm  # Use AuthenticationForm
 
     def form_valid(self, form):
